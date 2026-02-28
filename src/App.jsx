3d5819fb7885f52c3f86b54054e1de2be789c184
subhalaxmi-srichandan08 <that-email@example.com>
2026-02-28 17:11:32 +0530

    wip: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Chat } from "./components/Chat/Chat";
+import { Assistant } from "./components/Assistant/Assistant";
+import { Theme } from "./components/Theme/Theme";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [assistant, setAssistant] = useState();
+  const [chats, setChats] = useState([]);
+  const [activeChatId, setActiveChatId] = useState();
+  const activeChatMessages = useMemo(
+    () => chats.find(({ id }) => id === activeChatId)?.messages ?? [],
+    [chats, activeChatId]
+  );
+
+  useEffect(() => {
+    handleNewChatCreate();
+  }, []);
+
+  function handleAssistantChange(newAssistant) {
+    setAssistant(newAssistant);
+  }
+
+  function handleChatMessagesUpdate(id, messages) {
+    const title = messages[0]?.content.split(" ").slice(0, 7).join(" ");
+
+    setChats((prevChats) =>
+      prevChats.map((chat) =>
+        chat.id === id
+          ? { ...chat, title: chat.title ?? title, messages }
+          : chat
+      )
+    );
+  }
+
+  function handleNewChatCreate() {
+    const id = uuidv4();
+
+    setActiveChatId(id);
+    setChats((prevChats) => [...prevChats, { id, messages: [] }]);
+  }
+
+  function handleActiveChatIdChange(id) {
+    setActiveChatId(id);
+    setChats((prevChats) =>
+      prevChats.filter(({ messages }) => messages.length > 0)
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+
+      <div className={styles.Content}>
+        <Sidebar
+          chats={chats}
+          activeChatId={activeChatId}
+          activeChatMessages={activeChatMessages}
+          onActiveChatIdChange={handleActiveChatIdChange}
+          onNewChatCreate={handleNewChatCreate}
+        />
+
+        <main className={styles.Main}>
+          {chats.map((chat) => (
+            <Chat
+              key={chat.id}
+              assistant={assistant}
+              isActive={chat.id === activeChatId}
+              chatId={chat.id}
+              chatMessages={chat.messages}
+              onChatMessagesUpdate={handleChatMessagesUpdate}
+            />
+          ))}
+
+          <div className={styles.Configuration}>
+            <Assistant onAssistantChange={handleAssistantChange} />
+            <Theme />
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
