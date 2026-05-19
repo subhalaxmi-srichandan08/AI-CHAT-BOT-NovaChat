49598dd91ec2b89d6d41dcacab38c21413d2819d
Subhalaxmi Srichandan <subhalaxmisrichandan08@gmail.com>
2026-05-19 15:42:21 +0530

    redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Testing
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
